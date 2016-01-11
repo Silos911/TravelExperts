ea7b399242cd3dfac830be082a859221e3361354
Stephen Strome <sStrome@hotmail.com>
2016-01-11 12:20:11 -0700

    Something
    ?S

---
+++ TravelExperts/TravelExpertsDB.cs
@@ -0,0 +1,35 @@
+﻿/*
+    Title:              TravelExpertsDB Class
+    Author:             Stephen Strome
+    Last Date Updated:  January 11, 2016
+
+    This class will return a connection to the TravelExperts database. It reads one line
+    from databasestring.txt to know where to connect to, then establishes a connection.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelExperts {
+    class TravelExpertsDB {
+        public static SqlConnection GetConnection() {
+            try {
+                //Read a line from where your connection string should be stored
+                StreamReader file = new StreamReader("databasestring.txt");
+                string databaseConnection = file.ReadLine();
+
+                //Testing testing testing please delete
+                SqlConnection connect = new SqlConnection(databaseConnection);
+                return connect;
+            }
+            catch(Exception e) {
+                throw e;
+            }
+        }
+    }
+}
