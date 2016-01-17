329ee11f71214bd617b4f23d2c3ab6c651026e59
dcerdas <deya.cerdas@gmail.com>
2016-01-16 20:28:25 -0700

    Keep working on Products_SuppliersDB
    Keep coding

--- TravelExperts/Products_SuppliersDB.cs
+++ TravelExperts/Products_SuppliersDB.cs
@@ -14,12 +14,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
 
 namespace TravelExperts
 {
     class Products_SuppliersDB
     {
+        public static Products_Suppliers GetProductsSuppliers(string productsSuppliersId)
+        {
+            //Gets connection data
+            SqlConnection connection = TravelExpertsDB.GetConnection();
+            //Defining statement to retrieve info using connection above
+            string selectStatement =
+                "SELECT ProductSupplierId, ProductId, SupplierId " +
+                "FROM Products_Suppliers " +
+                "WHERE ProductSupplierId = @ProductSupplierId";
+            SqlCommand selectCommand = new SqlCommand(selectStatement, connection);
+            selectCommand.Parameters.AddWithValue("@ProductSupplierId", productSupplierId);
+            try
+            {
 
+            }
+
+        
     }
 }
 
